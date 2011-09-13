7e2ca302106b9e1d857e420449da699f018d6f9e
jsplawn <jsplawn@gmail.com>
2011-09-12 23:57:31 -0700

    refactoring and adding ruby commands

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
 
 describe PagesController do
-	render_views
+  render_views
+
+  before(:each) do
+   @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-    
+
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-      				:content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => @base_title + " | Home")
     end
   end
 
@@ -20,26 +25,37 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
-    
+
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-      				:content => 
-      "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => @base_title + " | Contact")
     end
   end
-  
-  describe "Get 'about'" do
+
+  describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
-    
-    it "Should be successful" do
+
+    it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-      				:content =>
-      "Ruby on Rails Tutorial Sample App | About")
+                                    :content => @base_title + " | About")
     end
+  end
+  
+  describe "GET 'help'" do
+      it "should be successful" do
+        get 'help'
+        response.should be_success
+      end
+  
+      it "should have the right title" do
+        get 'help'
+        response.should have_selector("title",
+                                      :content => @base_title + " | Help")
+      end
   end
 end
