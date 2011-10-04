959e3b7b16c97285d41027d2bd7f709cfbbe9249
jsplawn <jsplawn@gmail.com>
2011-10-04 13:53:27 -0700

    updatesbeforeC6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+	def logo
+	  image_tag("logo.png", :alt => "Sample App", :class => "round") 
+	end
+	
+	def rail1
+	   image_tag("rails.png", :alt => "Sample App", :class => "round") 
+	end
+	
+	
 	# Return a title on a per-page basis.
 	def title
 	  base_title = "Ruby on Rails Tutorial Sample App"
