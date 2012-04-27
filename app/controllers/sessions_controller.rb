8bf5c8aaf6fa4c3d8029dd2e32f8a3b1985afc83
jsplawn <jsplawn@gmail.com>
2012-04-26 19:13:25 -0700

    flash error fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:session][:email], 
     				params[:session][:password])
     if user.nil?
-      flash.now[:error] = "Invalid email/password combination."
+      flash.now[:error] = 'Invalid email/password combination.'
       @title = "Sign in"
       render 'new'
     else
