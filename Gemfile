0a4881420bf0ecb9cc27289f97f248f3d14c9436
jsplawn <jsplawn@gmail.com>
2012-05-05 09:20:53 -0700

    Finished User Model

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.0'
 gem 'gravatar_image_tag', '1.0.0.pre2'
+gem "will_paginate", "~> 3.0.2"
 
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120503190650) do
+ActiveRecord::Schema.define(:version => 20120505154318) do
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20120503190650) do
     t.datetime "updated_at"
     t.string   "encrypted_password"
     t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
